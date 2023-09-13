076356e24e8700feb2c82808434952d9b6a5570a
Muhammad Ali Hasan <alihxn23@gmail.com>
2023-09-12 21:31:27 -0500

    add RankItems.js and added React markup to it ;

---
+++ RankingApp/ClientApp/src/components/RankItems.js
@@ -0,0 +1,19 @@
+﻿import React, { useState, useEffect } from 'react';
+
+const RankItems = () => {
+    const [items, setItems] = useState([]);
+
+    const dataType = 1;
+
+    useEffect(() => {
+        fetch(`item/${dataType}`)
+            .then(results => results.json())
+            .then(jsonResult => setItems(jsonResult))
+    }, [])
+
+    return (
+        <main>
+            (items !== null) ? items.map(item => <h3>item.title</h3>) : <div>loading ...</div>
+        </main>
+    )
+}
