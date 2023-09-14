8beab136354d53765a2695879b9aca2d8f628d67
Muhammad Ali Hasan <alihxn23@gmail.com>
2023-09-13 20:56:16 -0500

    add more content to RankingGrid.js and display the component in RankItems

--- RankingApp/ClientApp/src/components/RankItems.js
+++ RankingApp/ClientApp/src/components/RankItems.js
@@ -1,5 +1,6 @@
 ﻿import React, { useState, useEffect } from 'react';
 import MovieImageArr from './MovieImages';
+import RankingGrid from './RankingGrid';
 
 const RankItems = () => {
     const [items, setItems] = useState([]);
@@ -14,6 +15,7 @@ const RankItems = () => {
 
     return (
         <main>
+            <RankingGrid items={items} imgArr={ MovieImageArr} />
             <div className="items-not-ranked">
                 {(items.length > 0) ? items.map(item =>
                     <div className="unranked-cell">
--- RankingApp/ClientApp/src/components/RankingGrid.js
+++ RankingApp/ClientApp/src/components/RankingGrid.js
@@ -7,7 +7,68 @@ const RankingGrid = ({ items, imgArr }) => {
     const cellCollectionBottom = [];
     const cellCollectionWorst = [];
 
+    const pushCellMarkupToArr = (cellCollection, rankNum, rowLabel) => {
+        if (rankNum > 0) {
+            let item = items.find(i => i.ranking === rankNum);
+            cellCollection.push(<div id={`rank-${rankNum}`} className="rank-cell"></div>)
+        }
+        else {
+            cellCollection.push(<div className="row-label">
+                <h4>{rowLabel}</h4>
+            </div>)
+        }
+    }
 
+    const createCellsForRow = (row) => {
+        let rankNum = 0;
+        let currCollection = [];
+        let label = "";
+        let numCells = 5;
+
+        for (let i = 1; i <= numCells; i++) {
+            rankNum = (i === 1) ? 0 : (numCells * (row - 1)) + i - row;
+
+            switch (row) {
+                case 1:
+                    currCollection = cellCollectionTop;
+                    label = "Top Tier";
+                    break;
+                case 2:
+                    currCollection = cellCollectionMiddle;
+                    label = "Middle Tier";
+                    break;
+                case 3:
+                    currCollection = cellCollectionBottom;
+                    label = "Bottom Tier";
+                    break;
+                case 4:
+                    currCollection = cellCollectionWorst;
+                    label = "Worst Tier";
+                    break;
+                default:
+                    break;
+            }
+
+            pushCellMarkupToArr(currCollection, rankNum, label)
+        }
+
+    }
+
+    const createCellsForRows = () => {
+        const maxRows = 4;
+        for (let row = 1; row <= maxRows; row++) {
+            createCellsForRow(row)
+        }
+    }
+
+    const createRowsForGrid = () => {
+        rankingGrid.push(<div className="rank-row top-tier">{cellCollectionTop}</div>)
+        rankingGrid.push(<div className="rank-row middle-tier">{cellCollectionMiddle}</div>)
+        rankingGrid.push(<div className="rank-row bottom-tier">{cellCollectionBottom}</div>)
+        rankingGrid.push(<div className="rank-row worst-tier">{cellCollectionWorst}</div>)
+
+        return rankingGrid
+    }
 
     const createRankingGrid = () => {
         createCellsForRows();
