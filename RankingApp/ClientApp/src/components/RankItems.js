316b7b68e8378390addde73591d0b5ea87b837f5
Muhammad Ali Hasan <alihxn23@gmail.com>
2023-09-13 21:27:21 -0500

    implement drag and drop methods

--- RankingApp/ClientApp/src/components/RankItems.js
+++ RankingApp/ClientApp/src/components/RankItems.js
@@ -5,6 +5,30 @@ import RankingGrid from './RankingGrid';
 const RankItems = () => {
     const [items, setItems] = useState([]);
 
+    const drag = (ev) => {
+        ev.dataTransfer.setData("text", ev.target.id);
+    }
+
+    const allowDrop = (ev) => { ev.preventDefault() }
+
+    const drop = (ev) => {
+        ev.preventDefault();
+        const targetElem = ev.target;
+        if (targetElem.nodeName === 'IMG') {
+            return false
+        }
+        if (targetElem.childNodes.length === 0) {
+            let data = parseInt(ev.dataTransfer.getData("text").subString(5))
+            const transformedCollection = items.map(item =>
+                item.id === parseInt(data) ?
+                    { ...item, ranking: parseInt(targetElem.id.subString(5)) }
+                    :
+                    { ...item, ranking: item.ranking }
+            )
+            setItems(transformedCollection);
+        }
+    }
+
     const dataType = 1;
 
     useEffect(() => {
@@ -15,7 +39,7 @@ const RankItems = () => {
 
     return (
         <main>
-            <RankingGrid items={items} imgArr={ MovieImageArr} />
+            <RankingGrid items={items} imgArr={MovieImageArr} />
             <div className="items-not-ranked">
                 {(items.length > 0) ? items.map(item =>
                     <div className="unranked-cell">
