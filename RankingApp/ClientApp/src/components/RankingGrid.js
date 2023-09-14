d77948ff04790b1a1da62bf3643882e1a02c414f
Muhammad Ali Hasan <alihxn23@gmail.com>
2023-09-13 20:43:34 -0500

    create RankingGrid.js and some markup and functions

---
+++ RankingApp/ClientApp/src/components/RankingGrid.js
@@ -0,0 +1,24 @@
+﻿
+const RankingGrid = ({ items, imgArr }) => {
+
+    const rankingGrid = [];
+    const cellCollectionTop = [];
+    const cellCollectionMiddle = [];
+    const cellCollectionBottom = [];
+    const cellCollectionWorst = [];
+
+
+
+    const createRankingGrid = () => {
+        createCellsForRows();
+        return createRowsForGrid();
+    }
+
+    return (
+        <div className="rankings">
+            {createRankingGrid()}
+        </div>
+    )
+}
+
+export default RankingGrid;
